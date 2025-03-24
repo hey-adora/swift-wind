9eae5773cf1f045cca0546278c8b52b9548d7999
hey_adora <146812294+hey-adora@users.noreply.github.com>
2025-03-24 22:26:23 +0200

    feat: added freya ui

---
+++ client-desktop/src/main.rs
@@ -0,0 +1,24 @@
+use freya::prelude::*;
+
+fn main() {
+    launch(app);
+}
+
+fn app() -> Element {
+    rsx!(
+        rect {
+            height: "100%",
+            width: "100%",
+            main_align: "center",
+            cross_align: "center",
+            background: "rgb(255, 0, 0)",
+            color: "white",
+            shadow: "0 4 20 5 rgb(0, 0, 0, 80)",
+            label {
+                font_size: "75",
+                font_weight: "bold",
+                "hey >:3"
+            }
+        }
+    )
+}
